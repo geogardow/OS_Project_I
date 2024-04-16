64e0aa1ae35f722e852b06d6d56f769301d6e53d
dmejias08 <dmejias08@estudiantec.cr>
2024-04-15 19:12:36 -0600

    get character in a certain position

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -2,27 +2,56 @@
 
 int main()
 {
-    read_char();
+    int char_pos = get_char_pos();
+    read_char(char_pos);
     return 0;
 }
 
-int read_char(){
-    char *filename = "readme.txt";
-    FILE *fp = fopen(filename, "r");
+int get_char_pos(){
+    return 9;
+}
 
-    if (fp == NULL)
-    {
-        printf("Error: could not open file %s", filename);
+void write_char_pos_mem(int current_char_pos){
+    printf("The number is: %d\n", current_char_pos);
+}
+
+void update_read_flag_to_true(){
+    printf("True");
+}
+
+
+int read_char(int char_pos){
+    const char* filename = "readme.txt";
+    FILE* fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error: Could not open file %s\n", filename);
         return 1;
     }
 
-    // read one character at a time and
-    // display it to the output
-    char ch;
-    while ((ch = fgetc(fp)) != EOF)
-        putchar(ch);
+    char_pos++;
 
-    // close the file
+    // Move the file pointer to the 100th character (offset 99)
+    if (fseek(fp, char_pos, SEEK_SET) != 0) {
+        printf("Error: fseek failed\n");
+        fclose(fp);
+        return 1;
+    }
+
+    // Read the character at the current file position
+    int ch = fgetc(fp);
+    if (ch == EOF) {
+        printf("Error: End of file reached\n");
+        update_read_flag_to_true();
+        fclose(fp);
+        return 1;
+    }
+
+    write_char_pos_mem(char_pos);
+    
+    printf("The character in the file is: %c\n", (char)ch);
+
+    // Close the file
     fclose(fp);
 
     return 0;
