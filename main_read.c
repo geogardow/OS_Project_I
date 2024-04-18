4f23502c8751780ac8b2883d0c09a12b481083fe
Geovanny Garcia Downing <85602062+geogardow@users.noreply.github.com>
2024-04-17 20:20:39 -0600

    Tests

--- main_read.c
+++ main_read.c
@@ -8,5 +8,13 @@ int main(){
     struct buffer_data data;
     data = read_from_buffer(3);
     printf("Char: %c, Date: %s, Time: %s\n", data.character, data.date, data.time);
+
+    struct stats_data stats;
+    stats = read_from_stats();
+    printf("Total characters: %d, Total words: %d\n", stats.chars_in_buffer, stats.transferred_chars);
+
+    struct mem_data mem;
+    mem = read_from_mem_data();
+    printf("Buffer size: %d, Read Counter: %d, Read Flag: %c\n", mem.buffer_size, mem.read_counter, mem.read_flag);
     return 0;
 }
